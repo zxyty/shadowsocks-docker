3cae361176c30e26d1eeb06992fddf137ccb7ff9
zxyty <zxy@zxyty.com>
2019-07-29 20:45:08 +0800

    Update shadowsocks.config.sh

--- shadowsocks.config.sh
+++ shadowsocks.config.sh
@@ -5,7 +5,7 @@ cat > /data/shadowsocks/server.json << EOF
     "server_port": ${SERVER_PORT-6550},
     "local_address": "127.0.0.1",
     "local_port": 1080,
-    "password": "${SERVER_PASSWORD-uljshdcULJSHDCty}",
+    "password": "${SERVER_PASSWORD-xxxxxx}",
     "timeout": 300,
     "method": "${SERVER_METHOD-rc4-md5}"
 }
@@ -17,7 +17,7 @@ cat > /data/shadowsocks/broswer.json << EOF
     "server_port": ${SERVER_PORT-6550},
     "local_address": "${SERVER_HOST}",
     "local_port": ${BROSWER_PROT-6551},
-    "password": "${SERVER_PASSWORD-uljshdcULJSHDCty}",
+    "password": "${SERVER_PASSWORD-xxxxxx}",
     "timeout": 300,
     "method": "${SERVER_METHOD-rc4-md5}"
 }
